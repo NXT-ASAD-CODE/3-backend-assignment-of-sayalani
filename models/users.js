bc797ced29856373a803d216a2c3380bd0f554ab
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-07 15:18:58 +0500

    making fileSystem

--- models/users.js
+++ models/users.js
@@ -2,3 +2,23 @@ const fs = require('fs')
 const path = require("path")
 const bcrypt = require("bcrypt")
 const Filepath = path.join(process.cwd(),"data","users.json")
+const readData =()=> {
+    return new Promise((resolve,reject)=>{
+        fs.readFile(Filepath,(err,data)=>{
+            if(err){
+                return reject(err)
+            }
+            resolve(JSON.parse(data.toString()))
+        })
+    })
+}
+const writeData =()=> {
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(Filepath,JSON.stringify(data),(err)=>{
+            if(err){
+                return reject(err)
+            }
+            resolve();
+        })
+    })
+}
