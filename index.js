a0e7feb493a58520bcd81a90b73fe17bdd9f0197
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-08 16:09:46 +0500

    Making controller and installing jws package

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const form = require("./routes/form.js");
 const port = 4000;
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use('/form',form)
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
--- routes/form.js
+++ routes/form.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const router = express.Router();
 router.post("/login",async(req,res)=>{
-
+try {
+    // const resp = await
+} catch (error) {
+    
+}
 })
 router.post("/signup",async(req,res)=>{
-
+try {
+    
+} catch (error) {
+    
+}
 })
 module.exports = router;
