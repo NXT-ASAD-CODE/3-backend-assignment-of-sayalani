e4e24e17397c5069bea4de2b86b9293289fc6a52
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-08 20:18:13 +0500

    Making controller file ready

--- controller/auth.js
+++ controller/auth.js
@@ -1,1 +1,24 @@
+const {createUser,findUser}= require("../models/users")
 const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const {SECRET_KEY} = require("../data/key");
+exports.createUser = async(email,password)=>{
+    try {
+        await createUser(email,password)
+    } catch (error) {
+        throw error
+    }
+}
+exports.login = async(email,password)=>{
+    try {
+        await findUser(email);
+        const result = await bcrypt.compare(password,user.password)
+        if(result){
+            var token = jwt.sign({email},SECRET_KEY);
+             return{token}
+        }
+       return "invalid email or password"
+    } catch (error) {
+        throw error
+    }
+}
